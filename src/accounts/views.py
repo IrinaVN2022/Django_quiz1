f549f5792f6f31bbb315ec1af78f50a9a837802c
Iryna Voznesenska <gloriya.od@ukr.net>
2023-04-29 21:49:32 +0300

    Flake8 issues fixed

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -8,9 +8,10 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.views.generic import UpdateView, FormView
+from django.views.generic import FormView
+from django.views.generic import UpdateView
 
-from .forms import UserRegisterForm, UserUpdateForm, UserReactivationForm
+from .forms import UserReactivationForm, UserRegisterForm, UserUpdateForm
 from .utils import signer
 
 
--- src/core/urls.py
+++ src/core/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView,  PasswordResetCompleteView, \
-    PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView
+from django.contrib.auth.views import PasswordChangeDoneView
+# from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView,  PasswordResetCompleteView, \
+#    PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import PasswordResetCompleteView
+from django.contrib.auth.views import PasswordResetConfirmView
+from django.contrib.auth.views import PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetView
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
