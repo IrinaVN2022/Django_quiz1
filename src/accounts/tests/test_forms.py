04de00369fe66947098988375eed8dfb9a844012
Iryna Voznesenska <gloriya.od@ukr.net>
2023-04-30 21:02:45 +0300

    Added tests

--- src/accounts/tests/test_forms.py
+++ src/accounts/tests/test_forms.py
@@ -1,6 +1,9 @@
-from accounts.forms import UserRegisterForm
 
+from django.forms import EmailField, ImageField, DateField, DateInput
+from accounts.forms import UserRegisterForm, UserReactivationForm, UserUpdateForm
 from django.test import TestCase
+from accounts.models import User
+from accounts.validators import validate_email_exist
 
 
 class TestForms(TestCase):
@@ -25,3 +28,92 @@ class TestForms(TestCase):
         form = UserRegisterForm(data={})
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form.errors), 4)
+
+
+class TestUserUpdateForm(TestCase):
+    def setUp(self) -> None:
+        self.data = {
+            'username': 'user',
+            'email': 'user@test.com',
+        }
+        self.not_required_data = {
+            'firs_name': 'user_first_name',
+            'last_name': 'user_last_name',
+            'city': 'Odesa',
+        }
+        self.form = UserUpdateForm
+
+    def test_birthday_field(self):
+        form = self.form(self.data)
+        field = form.fields['birthday']
+        self.assertIsInstance(field, DateField)
+        self.assertFalse(field.required)
+        self.assertIsInstance(field.widget, DateInput)
+
+    def test_email_field(self):
+        form = self.form(self.data)
+        field = form.fields['email']
+        self.assertIsInstance(field, EmailField)
+        self.assertTrue(field.required)
+
+    def test_form_valid_data(self):
+        # required data
+        form = self.form(data=self.data)
+        self.assertTrue(form.is_valid())
+
+        # full data
+        self.data.update(self.not_required_data)
+        form = self.form(data=self.data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid_data(self):
+        # no data
+        form = self.form(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(2, len(form.errors))
+
+        # not required data
+        form = self.form(data=self.not_required_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(2, len(form.errors))
+
+
+class TestReactivationForm(TestCase):
+    def setUp(self) -> None:
+        self.data = {
+            'email': 'user@test.com',
+        }
+
+        User.objects.create(
+            username='user',
+            password='123qwe!@#',
+            email=self.data['email']
+        )
+
+        self.form = UserReactivationForm
+
+    def test_email_field(self):
+        form = self.form(data=self.data)
+        field = form.fields['email']
+        self.assertIsInstance(field, EmailField)
+        self.assertTrue(field.required)
+        self.assertIn(validate_email_exist, field.validators)
+
+    def test_valid_email(self):
+        form = self.form(data=self.data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_email(self):
+        form = self.form(data={'email': '1test@te.cm'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(1, len(form.errors))
+
+    def test_clear_email_of_exist_user(self):
+        form = self.form(data=self.data)
+        form.is_valid()
+        self.assertEqual(self.data['email'], form.clean_email())
+
+    def test_clear_email_of_not_exist_user(self):
+        form = self.form(data={'email': 'not_exist_user@test.com'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(1, len(form.errors))
--- src/accounts/tests/test_urls.py
+++ src/accounts/tests/test_urls.py
@@ -1,4 +1,6 @@
-from accounts.views import UserRegisterView
+from django.views.generic import TemplateView
+
+from accounts.views import UserRegisterView, UserReactivationView, UserLoginView, UserLogoutView, UserProfileUpdateView
 from accounts.views import user_activate
 from accounts.views import user_profile_view
 
@@ -20,3 +22,31 @@ class TestUrls(SimpleTestCase):
     def test_activate_user_url_resolves(self):
         url = reverse('accounts:register_activate', kwargs={'sign': 'ioewutrljc3409ur90u43ri3j4'})
         self.assertEqual(resolve(url).func, user_activate)
+
+    def test_register_done_url_resolve(self):
+        url = reverse('accounts:register_done')
+        self.assertIs(TemplateView, resolve(url).func.view_class)
+
+    def test_reactivation_url_resolve(self):
+        url = reverse('accounts:reactivation')
+        self.assertIs(UserReactivationView, resolve(url).func.view_class)
+
+    def test_reactivation_done_url_resolve(self):
+        url = reverse('accounts:reactivation_done')
+        self.assertIs(TemplateView, resolve(url).func.view_class)
+
+    def test_login_url_resolve(self):
+        url = reverse('accounts:login')
+        self.assertIs(UserLoginView, resolve(url).func.view_class)
+
+    def test_logout_url_resolve(self):
+        url = reverse('accounts:logout')
+        self.assertIs(UserLogoutView, resolve(url).func.view_class)
+
+    def test_profile_url_resolve(self):
+        url = reverse('accounts:profile')
+        self.assertIs(user_profile_view, resolve(url).func)
+
+    def test_profile_update_url_resolve(self):
+        url = reverse('accounts:profile_update')
+        self.assertIs(UserProfileUpdateView, resolve(url).func.view_class)
